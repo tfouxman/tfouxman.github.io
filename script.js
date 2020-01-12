dd79020342ea14b419b41933483ebb6b8ea10920
Tom Fouxman <tfouxman@gmail.com>
2020-01-12 14:22:45 -0500

    Added some scrollReveal cabalities to colour boxes and introduction

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
+let anchorlinks = document.querySelectorAll('a[href^="#"]');
+window.sr = ScrollReveal({reset: true});
 
 for (let item of anchorlinks) { // relitere 
     item.addEventListener('click', (e)=> {
@@ -11,3 +12,23 @@ for (let item of anchorlinks) { // relitere
         e.preventDefault()
     })
 }
+
+let boxContainer = document.getElementsByClassName('boxes');
+let boxes = [];
+
+for (var i = 0; i < boxContainer.length; i++) {
+    allBoxes(boxContainer[i]);
+}
+
+function allBoxes(node) {
+    for (var i = 0; i < node.childNodes.length; i++) {
+        var child = node.childNodes[i];
+        allBoxes(child);
+    }
+    if (node.tagName == 'DIV' && !node.firstChild) {
+        let duration = Math.random() * 2000;
+        sr.reveal(node, {duration: duration});
+    }
+}
+
+sr.reveal('.intro', { duration: 1000 });
