e715ce375ec23add018b745a35bab1d21dd58256
Tom Fouxman <tfouxman@gmail.com>
2020-01-12 22:15:27 -0500

    Added ScrollReveal.js animations

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let anchorlinks = document.querySelectorAll('a[href^="#"]');
 window.sr = ScrollReveal({reset: true});
 
+
+// jQuery to wait for the document to load before starting typing and revealing animations
 $(document).ready(function() {
-    console.log("meme");
-    $("#typewriter").addClass("typewriter").removeClass("hidden"); 
+    $("#typewriter").addClass("typewriter").removeClass("hidden");
+    sr.reveal('.banner-image', {duration: 3000, scale: 0});
 });
 
 for (let item of anchorlinks) {
@@ -25,22 +27,27 @@ for (var i = 0; i < boxContainer.length; i++) {
     allBoxes(boxContainer[i]);
 }
 
-function allBoxes(node) {
-    let config = {
-        enter: 'bottom',
-        wait: '0.5s',
-        move: '20px',
-        scale: { direction: 'up', power: '50%'}
-    }
+function allBoxes(node) {  
     for (var i = 0; i < node.childNodes.length; i++) {
         var child = node.childNodes[i];
         allBoxes(child);
     }
+    let minmax = [2, 4];
+    let sides = ['top', 'bottom', 'left', 'right']
     if (node.tagName == 'DIV' && !node.firstChild) {
-        let duration = Math.random() * 2000;
+        let side = sides[Math.floor(Math.random() * (3 - 0 + 1)) + 0];
+        let config = {
+            enter: 'bottom',
+            wait: '0.5s',
+            move: '20px',
+            scale: 0.5,
+            origin: side,
+            distance: '5px'
+        }
+        let duration = 1000 * (Math.floor(Math.random() * (minmax[1] - minmax[0] + 1)) + minmax[0]);
         config.duration = duration;
         sr.reveal(node, config);
     }
 }
 
-sr.reveal('.intro', { duration: 1000 });
+sr.reveal('.intro', { duration: 2200, scale: 0.7 });
