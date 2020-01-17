ba9eaf2b753e22c73ffe60efda811677f2e546b3
Tom Fouxman <tfouxman@gmail.com>
2020-01-17 18:12:07 -0500

    Major update to second container

--- script.js
+++ script.js
@@ -21,21 +21,26 @@ for (let item of anchorlinks) {
     })
 }
 
-let boxContainer = document.querySelectorAll('.boxes, .boxes-res, .boxes-intro');
+let boxContainer = document.querySelectorAll('.boxes-intro');
 let boxes = [];
 
 for (var i = 0; i < boxContainer.length; i++) {
     allBoxes(boxContainer[i]);
 }
 
-function allBoxes(node) {  
+let sides = ['top', 'bottom', 'left', 'right']
+
+function allBoxes(node) {
+    let sides = ['top', 'bottom', 'left', 'right']
+    let count = 0;
     for (var i = 0; i < node.childNodes.length; i++) {
         var child = node.childNodes[i];
         allBoxes(child);
     }
     let minmax = [2, 4];
-    let sides = ['top', 'bottom', 'left', 'right']
+    
     if ((node.tagName == 'DIV' && !node.firstChild) && node.nodeType != Node.TEXT_NODE) {
+        count++;
         let side = sides[Math.floor(Math.random() * (3 - 0 + 1)) + 0];
         let config = {
             scale: 0.5,
@@ -46,6 +51,20 @@ function allBoxes(node) {
         config.duration = duration;
         sr.reveal(node, config);
     }
+}
+
+let aboutItems = document.querySelectorAll('.about-container');
+for (var i = 0; i < aboutItems.length; i++) {
+    let side = sides[Math.floor(Math.random() * (i - 0 + 1)) + 0];
+    let config = {
+        scale: 0.5,
+        origin: side,
+        distance: '5px'
+    }
+    let duration = 1000 * (Math.floor(Math.random() * ((i/2) * 1.15 - 1 + 1)) + 1);
+    config.duration = duration;
+    sr.reveal(aboutItems[i], config);
+    
 }
 
 sr.reveal('.intro', { duration: 1400, scale: 0.7, origin: 'top', distance: '50px' });
