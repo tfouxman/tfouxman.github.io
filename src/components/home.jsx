e59783fa1417c525008c1a8b58f0fc078de63543
Tom Fouxman <tfouxman@gmail.com>
2020-05-09 03:10:52 -0400

    Fixed typewriter effect to work responsively

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import Jumbotron from "./jumbotron";
 import styled from "styled-components";
+import Typed from "typed.js";
 
 const Styles = styled.div`
-  .typewriter {
-    margin: auto;
-    display: inline-block;
+  .typed-cursor {
+    font-size: 40px;
+    animation: typing 2s steps(20, end), blink-caret 0.75s step-end 5;
+    border-right: 0.15em solid transparent;
   }
 
-  .typewriter h2 {
-    overflow: hidden;
-    white-space: nowrap;
-    overflow-wrap: break-word;
-    border-right: 0.15em solid transparent;
-    letter-spacing: 0.1em;
-    animation: typing 2s steps(20, end), blink-caret 0.75s step-end 5;
+  .wrap {
+    height: 100px;
+  }
+
+  @media (max-width: 420px) {
+    .wrap {
+      height: 150px;
+    }
   }
 
   @-webkit-keyframes typing {
@@ -29,7 +32,7 @@ const Styles = styled.div`
   @-webkit-keyframes blink-caret {
     from,
     to {
-      border-color: orange;
+      border-color: #efefef;
     }
     50% {
       border-color: transparent;
@@ -50,7 +53,7 @@ const Styles = styled.div`
   @keyframes blink-caret {
     from,
     to {
-      border-color: orange;
+      border-color: #efefef;
     }
     50% {
       border-color: transparent;
@@ -59,28 +62,51 @@ const Styles = styled.div`
 `;
 
 class Home extends Component {
+  componentDidMount() {
+    this.strings = ["Tom Fouxman"];
+    if (window.outerWidth < 420) this.strings = ["Tom<br>Fouxman"];
+    const options = {
+      strings: this.strings,
+      typeSpeed: 150,
+      autoInsertCss: false,
+      cursorChar: "",
+    };
+
+    this.typed = new Typed(this.el, options);
+  }
+
+  componentWillUnmount() {
+    this.typed.destroy();
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Jumbotron>
-          <Styles>
-            <div className="typewriter">
-              <h2 className="display-4">Tom Fouxman</h2>
+        <Styles>
+          <Jumbotron>
+            <div className="wrap">
+              <span
+                className="display-4"
+                style={{ whiteSpace: "pre-wrap" }}
+                ref={(el) => {
+                  this.el = el;
+                }}
+              ></span>
             </div>
-          </Styles>
-          <h2>Home</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Repudiandae molestias laborum voluptatibus fugiat maxime blanditiis
-            accusantium iusto aliquid? Dolores exercitationem vel hic et dolore
-            consequatur saepe veniam obcaecati facere odit?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-            quibusdam neque ducimus est qui odio, molestias totam consequuntur
-            maiores voluptates!
-          </p>
-        </Jumbotron>
+            <h2>Home</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Repudiandae molestias laborum voluptatibus fugiat maxime
+              blanditiis accusantium iusto aliquid? Dolores exercitationem vel
+              hic et dolore consequatur saepe veniam obcaecati facere odit?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatum quibusdam neque ducimus est qui odio, molestias totam
+              consequuntur maiores voluptates!
+            </p>
+          </Jumbotron>
+        </Styles>
       </React.Fragment>
     );
   }
